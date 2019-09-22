f55d567b270cfe17a4f6551052a9c9d77f0aff68
Abhijeet Singh <redefinedabhi2@gmail.com>
2019-09-23 02:20:47 +0530

    Json parsing code completed for the detail view screen

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,65 @@
 package com.udacity.sandwichclub.utils;
 
+import android.text.TextUtils;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
+    public static Sandwich parseSandwichJson(String sandwichJSON) {
+
+        List<String> alsoKnownAsList;
+        List<String> ingredientsList;
+        Sandwich sandwich;
+
+        if (TextUtils.isEmpty(sandwichJSON)) {
+            return null;
+        }
+
+        try {
+            sandwich = new Sandwich();
+            JSONObject baseJsonResponse = new JSONObject(sandwichJSON);
+            JSONObject name = baseJsonResponse.getJSONObject("name");
+            String sandwichName = name.getString("mainName");
+            sandwich.setMainName(sandwichName);
+
+            JSONArray alsoKnownAsArray = name.getJSONArray("alsoKnownAs");
+            alsoKnownAsList = new ArrayList<>();
+            if(alsoKnownAsArray.length()>0){
+                for(int i=0;i<alsoKnownAsArray.length();i++){
+                    alsoKnownAsList.add(alsoKnownAsArray.getString(i));
+                }
+            }
+
+            String placeOfOriginString = baseJsonResponse.getString("placeOfOrigin");
+            sandwich.setPlaceOfOrigin(placeOfOriginString);
+
+            String description = baseJsonResponse.getString("description");
+            sandwich.setDescription(description);
+
+            String image = baseJsonResponse.getString("image");
+            sandwich.setImage(image);
+
+            JSONArray ingredients = baseJsonResponse.getJSONArray("ingredients");
+
+            ingredientsList = new ArrayList<>();
+            if(ingredients.length()>0){
+                for(int i=0;i<ingredients.length();i++){
+                    ingredientsList.add(ingredients.getString(i));
+                }
+            }
+            sandwich.setIngredients(ingredientsList);
+            return sandwich;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
